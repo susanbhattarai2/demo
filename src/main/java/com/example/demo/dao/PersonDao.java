829475f8ed93cbccc0606bdd21575fbf871adf6e
Susan <susanbhattarai36@gmail.com>
2021-05-19 13:15:25 +0545

    filter person by id

--- src/main/java/com/example/demo/dao/PersonDao.java
+++ src/main/java/com/example/demo/dao/PersonDao.java
@@ -3,9 +3,11 @@ package com.example.demo.dao;
 import com.example.demo.model.Person;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PersonDao {
+
     int insertPerson(UUID id, Person person);
 
     default int insertPerson(Person person) {
@@ -14,4 +16,10 @@ public interface PersonDao {
     }
 
     List<Person> selectAllPeople();
+
+    Optional<Person> selectPersonById(UUID id);
+
+    int deletePersonById(UUID id);
+
+    int updatePersonById(UUID id , Person person);
 }
