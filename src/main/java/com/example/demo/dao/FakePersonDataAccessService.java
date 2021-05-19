b6a2c5ba6044c8267eb70dfc037978cb7fd8e4e9
Susan <susanbhattarai36@gmail.com>
2021-05-19 12:40:53 +0545

    add person complete

--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -2,10 +2,14 @@ package com.example.demo.dao;
 
 import com.example.demo.model.Person;
 
+import org.springframework.stereotype.Repository;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+
+@Repository("fakeDao")
 public class FakePersonDataAccessService implements PersonDao{
 
     private static List<Person> DB = new ArrayList<>();
@@ -14,5 +18,10 @@ public class FakePersonDataAccessService implements PersonDao{
     public int insertPerson(UUID id, Person person) {
         DB.add(new Person(id, person.getName()));
         return 1;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return DB;
     }
 }
